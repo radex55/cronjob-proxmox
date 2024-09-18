45c1b13a48de7fd59a85bce22f2fc1e40145dff0
Radex55 <radex55@users.noreply.github.com>
2024-09-18 12:15:41 +0700

    Create check_battery.sh

---
+++ check_battery.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Cek status charger ("Discharging || FULL")
+CHARGER_STATUS=$(cat /sys/class/power_supply/BAT1/status)
+
+# Cek kapasitas baterai
+BATTERY_CAPACITY=$(cat /sys/class/power_supply/BAT1/capacity)
+
+# File marker untuk memastikan cron hanya dibuat sekali
+MARKER_FILE="/tmp/cron_marker"
+
+# File marker smart switch untuk memastikan cron hanya dibuat sekali
+MARKER_FILE_SWITCH="/tmp/cron_marker_switch"
+
+# Jika kapasitas baterai di bawah 61% turn on smart switch
+if [ "$BATTERY_CAPACITY" -lt 61 ] && [ ! -f "$MARKER_FILE_SWITCH" ]; then
+
+    # Buat file marker smart switch
+    touch "$MARKER_FILE_SWITCH"
+
+    # Jalankan perintah
+    /home/switch_on.sh
+fi
+
+# Jika kapasitas baterai di atas 85% maka turn off smart switch
+if [ "$BATTERY_CAPACITY" -gt 85 ] && [ -f "$MARKER_FILE_SWITCH" ]; then
+
+    # Hapus marker smart switch file
+    rm "$MARKER_FILE_SWITCH"
+
+    # Jalankan perintah bash
+    /home/switch_off.sh
+fi
+
+# Jika charger tidak terhubung dan kapasitas baterai di bawah 55% dan marker belum dibuat
+if [ "$CHARGER_STATUS" == "Discharging" ] && [ "$BATTERY_CAPACITY" -lt 55 ] && [ ! -f "$MARKER_FILE" ]; then
+
+    # Buat file marker
+    touch "$MARKER_FILE"
+
+    # Tambahkan cron job
+    (crontab -l 2>/dev/null; echo "* * * * * /home/cron_script.sh") | crontab -
+    (crontab -l 2>/dev/null; echo "* * * * * /home/shutdown_if_battery_low.sh") | crontab -
+fi
+
+# charger terhubung ("Full")
+if [ "$CHARGER_STATUS" == "Full" ]; then
+
+    # Hapus marker smart switch file
+    rm "$MARKER_FILE_SWITCH"
+
+    # Jalankan perintah bash
+    /home/switch_off.sh
+fi
