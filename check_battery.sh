384dd55d2e16c45eb6190664d6599cc1198df057
Radex55 <radex55@users.noreply.github.com>
2024-10-23 10:52:31 +0700

    Update check_battery.sh

--- check_battery.sh
+++ check_battery.sh
@@ -12,14 +12,28 @@ MARKER_FILE="/tmp/cron_marker"
 # File marker smart switch untuk memastikan cron hanya dibuat sekali
 MARKER_FILE_SWITCH="/tmp/cron_marker_switch"
 
-# Jika kapasitas baterai di bawah 61% turn on smart switch
-if [ "$BATTERY_CAPACITY" -lt 61 ] && [ ! -f "$MARKER_FILE_SWITCH" ]; then
+# charger terhubung ("Full")
+if [ "$CHARGER_STATUS" == "Full" ]; then
+          
+    # Hapus marker smart switch file
+    if [ -f "$MARKER_FILE_SWITCH" ]; then
+        rm "$MARKER_FILE_SWITCH"
+    fi
 
-    # Buat file marker smart switch
-    touch "$MARKER_FILE_SWITCH"
+    # Jalankan perintah bash
+    /home/switch_off.sh
+fi
 
-    # Jalankan perintah
-    /home/switch_on.sh
+# Jika kapasitas baterai di atas 92% maka turn off smart switch
+if [ "$BATTERY_CAPACITY" -gt 92 ] && [ "$CHARGER_STATUS" == "Charging" ]; then
+
+    # Hapus marker smart switch file
+    if [ -f "$MARKER_FILE_SWITCH" ]; then
+        rm "$MARKER_FILE_SWITCH"
+    fi
+
+    # Jalankan perintah bash
+    /home/switch_off.sh
 fi
 
 # Jika kapasitas baterai di atas 85% maka turn off smart switch
@@ -32,6 +46,16 @@ if [ "$BATTERY_CAPACITY" -gt 85 ] && [ -f "$MARKER_FILE_SWITCH" ]; then
     /home/switch_off.sh
 fi
 
+# Jika kapasitas baterai di bawah 61% turn on smart switch
+if [ "$BATTERY_CAPACITY" -lt 61 ] && [ ! -f "$MARKER_FILE_SWITCH" ]; then
+
+    # Buat file marker smart switch
+    touch "$MARKER_FILE_SWITCH"
+
+    # Jalankan perintah
+    /home/switch_on.sh
+fi
+
 # Jika charger tidak terhubung dan kapasitas baterai di bawah 55% dan marker belum dibuat
 if [ "$CHARGER_STATUS" == "Discharging" ] && [ "$BATTERY_CAPACITY" -lt 55 ] && [ ! -f "$MARKER_FILE" ]; then
 
@@ -41,16 +65,4 @@ if [ "$CHARGER_STATUS" == "Discharging" ] && [ "$BATTERY_CAPACITY" -lt 55 ] && [
     # Tambahkan cron job
     (crontab -l 2>/dev/null; echo "* * * * * /home/cron_script.sh") | crontab -
     (crontab -l 2>/dev/null; echo "* * * * * /home/shutdown_if_battery_low.sh") | crontab -
-fi
-
-# charger terhubung ("Full")
-if [ "$CHARGER_STATUS" == "Full" ]; then
-          
-    # Hapus marker smart switch file
-    if [ -f "$MARKER_FILE_SWITCH" ]; then
-        rm "$MARKER_FILE_SWITCH"
-    fi
-
-    # Jalankan perintah bash
-    /home/switch_off.sh
 fi
